ae9c7dab07d3aa84ab6cdeeef5bcd8b1bd4ed708
Gaurang Tandon <gaurang@blaze.today>
2022-03-25 19:25:22 +0530

    Improved printing

--- q2.py
+++ q2.py
@@ -1,8 +1,9 @@
+from multiprocessing import pool
 import numpy as np
 # import torch
 
 DIM = 3
-N = 3 # zero indexed nodes
+N = 4 # zero indexed nodes
 ITERS = 3
 
 def relu(vec):
@@ -15,14 +16,15 @@ def f(vec):
 
 adjmatrix = np.zeros((N, N), dtype=np.bool8)
 
-def singlestep(node_vectors_previous: np.ndarray):
+def singlestep(node_vectors_previous: np.ndarray, stage: int=0):
     node_vectors = np.zeros((N, DIM))
     W = np.random.random((DIM, DIM))
     B = np.random.random((DIM, DIM))
 
+    print(f"Stage {stage}\n-----\nW={W}\nB={B}\n")
     for node_idx in range(N):
         pooled_neighbours = adjmatrix[node_idx]
-        average_pool = node_vectors[pooled_neighbours].sum(axis=0)
+        average_pool = node_vectors_previous[pooled_neighbours].sum(axis=0)
         my_previous_vector = node_vectors_previous[node_idx]
         assert average_pool.shape == my_previous_vector.shape
         assert W.shape == (DIM, DIM)
@@ -34,18 +36,21 @@ def singlestep(node_vectors_previous: np.ndarray):
         assert neighbor_pool.shape == (DIM,)
         assert my_bias.shape == (DIM,)
         node_vectors[node_idx] = f(neighbor_pool + my_bias)
+        print(f"For node {node_idx}, neighbour pool: {neighbor_pool}; my bias: {my_bias}; new vector={node_vectors[node_idx]}")
+    
+    print(f"Final result\n{node_vectors}")
 
     return node_vectors
 
 def main():
     adjmatrix[0][1] = 1
     adjmatrix[1][2] = 1
+    adjmatrix[2][3] = 1
 
     node_vectors = np.random.random((N, DIM))
     print("Stage 0", node_vectors)
-    for i in range(ITERS):
-        node_vectors = singlestep(node_vectors)
-        print(f"Stage {i + 1}", node_vectors)
+    for stage in range(1, ITERS + 1):
+        node_vectors = singlestep(node_vectors, stage=stage)
 
 if __name__ == "__main__":
     main()
